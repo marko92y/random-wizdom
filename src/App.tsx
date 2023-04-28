b913b0715b56ba97c5ca5c0cace6351277ed7c6a
Marko Jelenkovic <marko92y@gmail.com>
2023-04-28 13:17:55 +0200

    added loading text

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from "react";
-import "./App.scss";
+import { Suspense } from "react";
 import Cursor from "./Components/Cursor";
 import { Canvas } from "@react-three/fiber";
 import Scene from "./Components/Scene";
 
 function App() {
   return (
-    <>
+    <Suspense
+      fallback={
+        <>
+          Loading...
+          <br />
+          Welcome
+        </>
+      }
+    >
       <Cursor />
       <Canvas>
         <Scene />
       </Canvas>
-    </>
+    </Suspense>
   );
 }
 
--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -1,5 +1,4 @@
 import { Environment, OrbitControls, PerspectiveCamera, Float} from '@react-three/drei';
-import { Suspense } from 'react';
 import Earth from "./Earth";
 import DutchShip from "./DutchShip";
 import Portal , {podcasts} from './Portal';
@@ -7,13 +6,13 @@ import Portal , {podcasts} from './Portal';
 
 function Scene() {
   return (
-    <Suspense fallback={null}>
-        <PerspectiveCamera far={10000}   target={[0,0, 0]} makeDefault position={[-60, 0, 120]} fov={50}/>
+        <>
+        <PerspectiveCamera  far={10000}   target={[0,0, 0]} makeDefault position={[-60, 0, 120]} fov={50}/>
         <OrbitControls 
+        enableZoom = {false}
         target={[0, 35, 0]}
         maxPolarAngle={Math.PI - Math.PI / 2}
         />
-
         <Environment
             files={process.env.PUBLIC_URL + '/textures/envmap.hdr'}
             background={"both"}
@@ -23,8 +22,7 @@ function Scene() {
         <DutchShip rotation={[0, 1, 0]} />
         </Float>
         {podcasts.map((podcast, index) => (<Portal key={index} podcast={podcast}/>))}
-        
-    </Suspense>
+        </>   
   )
 }
 
