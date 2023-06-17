f3c12810025e4d3b494197c2024eba4daa67a0fe
Marko Jelenkovic <marko92y@gmail.com>
2023-06-17 22:37:52 +0200

    helmet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
+import { Helmet } from "react-helmet";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,19 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7290248183564409"
+        crossorigin="anonymous"
+      ></script>
+      <meta charSet="utf-8" />
+      <title>My Title</title>
+      <meta
+        name="description"
+        content="Random Wisdom - Joe Rogan, David Lee Roth, Travis Barker, Chuck Palahniuk, Guy Ritchie, Andrew Huberman"
+      />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
